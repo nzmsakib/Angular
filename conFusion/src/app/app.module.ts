317acd35cd14ee085b7b47cbb4b57e984c77b9e9
Md Nazmus Sakib <nzmsakib@users.noreply.github.com>
2020-08-05 11:46:03 +0600

    basic services

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -9,11 +9,12 @@ import {MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
