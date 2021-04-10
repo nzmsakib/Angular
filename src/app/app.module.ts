3710beb2e0b55a8c84cb2ae93ca03fa21f44cdb3
Nazmus Sakib <60852942+nzmsakib@users.noreply.github.com>
2021-04-10 20:53:59 +0600

    single page application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { DishService } from "./services/dish.service";
+import { PromotionService } from "./services/promotion.service";
 
 import { AppComponent } from './app.component';
 
@@ -44,7 +45,10 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
