55aec7eeccfd14ad5b073997c4bf6ad613ece121
Nazmus Sakib <60852942+nzmsakib@users.noreply.github.com>
2021-04-10 23:20:58 +0600

    rxjs part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -22,7 +22,7 @@ export class DishdetailComponent implements OnInit {
     const id = +this.route.snapshot.params['id'];
     
     this.dishservice.getDish(id.toString())
-    .then(dish => this.dish = dish);
+    .subscribe(dish => this.dish = dish);
   }
 
   goBack(): void {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,13 +24,13 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-    .then(dish => this.dish = dish);
+    .subscribe(dish => this.dish = dish);
 
     this.promotionservice.getFeaturedPromotion()
-    .then(promo => this.promotion = promo);
+    .subscribe(promo => this.promotion = promo);
     
     this.leaderService.getFeaturedLeader()
-    .then(leader => this.leader = leader);
+    .subscribe(leader => this.leader = leader);
   }
 
 }
